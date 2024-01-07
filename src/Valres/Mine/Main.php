557ac05b93bc3a55c9fb49ca7eacb9cc6d0664ec
Valres <104681026+ValresMC@users.noreply.github.com>
2024-01-07 17:26:23 +0100

    Update Main.php

--- src/Valres/Mine/Main.php
+++ src/Valres/Mine/Main.php
@@ -3,7 +3,6 @@
 namespace Valres\Mine;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\Config;
 use pocketmine\utils\SingletonTrait;
 use Valres\Mine\Listeners\blockEvents;
 
@@ -15,16 +14,27 @@ class Main extends PluginBase
     {
         $this->saveDefaultConfig();
         $this->getLogger()->info("by Valres est lancé !");
-        $this->getServer()->getPluginManager()->registerEvents(new blockEvents($this), $this);
+        $this->getServer()->getPluginManager()->registerEvents(new blockEvents(), $this);
+
+        $config = $this->getConfig();
+
+        foreach($config->get("messages") as $type => $message){
+            if(!isset(blockEvents::$messages[$type])){
+                blockEvents::$messages[$type] = $message;
+            }
+        }
+        foreach($config->get("blocks") as $block => $pourcent){
+            if(!isset(blockEvents::$blocks[$block])){
+                blockEvents::$blocks[$block] = $pourcent;
+            }
+        }
+        var_dump(blockEvents::$blocks);
+        blockEvents::$world = $config->get("world");
+        blockEvents::$timer = $config->get("regen-time");
     }
 
     protected function onLoad(): void
     {
         self::setInstance($this);
-    }
-
-    public function config(): Config
-    {
-        return new Config($this->getDataFolder()."config.yml", Config::YAML);
     }
 }
