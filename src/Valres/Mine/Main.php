5b6aa32a1638fb65db69e9bc422f64c7985e308f
Valres <104681026+ValresMC@users.noreply.github.com>
2024-01-07 23:35:26 +0100

    Update Main.php

--- src/Valres/Mine/Main.php
+++ src/Valres/Mine/Main.php
@@ -2,7 +2,12 @@
 
 namespace Valres\Mine;
 
+use JsonException;
+use pocketmine\block\BlockTypeIds;
+use pocketmine\item\StringToItemParser;
 use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\ClosureTask;
+use pocketmine\utils\Config;
 use pocketmine\utils\SingletonTrait;
 use Valres\Mine\Listeners\blockEvents;
 
@@ -10,9 +15,13 @@ class Main extends PluginBase
 {
     use SingletonTrait;
 
+    /**
+     * @return void
+     */
     protected function onEnable(): void
     {
         $this->saveDefaultConfig();
+        $this->saveResource("data.yml");
         $this->getLogger()->info("by Valres est lancé !");
         $this->getServer()->getPluginManager()->registerEvents(new blockEvents(), $this);
 
@@ -28,10 +37,46 @@ class Main extends PluginBase
                 blockEvents::$blocks[$block] = $pourcent;
             }
         }
+        foreach($config->get("allowed-blocks") as $allowed){
+            $allowed_ = StringToItemParser::getInstance()->parse($allowed)->getBlock()->getTypeId();
+            if(!isset(blockEvents::$allowed[$allowed_])){
+                blockEvents::$allowed[] = $allowed_;
+            }
+        }
         blockEvents::$world = $config->get("world");
         blockEvents::$timer = $config->get("regen-time");
+
+        $this->getServer()->getWorldManager()->loadWorld(blockEvents::$world);
+        $this->getScheduler()->scheduleDelayedTask(new ClosureTask(function(){
+            $world = $this->getServer()->getWorldManager()->getWorldByName(blockEvents::$world);
+            $data = new Config($this->getDataFolder() . "data.yml", Config::YAML);
+            foreach($data->getAll() as $pos => $block){
+                $pos_ = explode(":", $pos);
+                $world->setBlockAt($pos_[0], $pos_[1], $pos_[2], StringToItemParser::getInstance()->parse(blockEvents::chooseBlock(blockEvents::$blocks))->getBlock());
+                $data->remove($pos);
+            }
+            $data->save();
+        }), 20);
+
     }
 
+    /**
+     * @return void
+     * @throws JsonException
+     */
+    protected function onDisable(): void
+    {
+        $data = new Config($this->getDataFolder() . "data.yml", Config::YAML);
+
+        foreach(blockEvents::$data as $pos => $block){
+            $data->set($pos, $block);
+        }
+        $data->save();
+    }
+
+    /**
+     * @return void
+     */
     protected function onLoad(): void
     {
         self::setInstance($this);
